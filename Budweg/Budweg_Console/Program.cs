ee017f948afbff54b2fb947bc8844ad254add9cb
Manse <malthe.h.2004@gmail.com>
2026-03-02 14:26:10 +0100

    conssle og det hele så i kan arbejde også på det

---
+++ Budweg/Budweg_Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Budweg_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
